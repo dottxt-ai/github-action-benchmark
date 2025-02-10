356086e9f10acba67ba4e82dc48c47ec4e40533e
Owen Shepherd <owen@owen.cafe>
2025-02-10 03:25:46 +0000

    Write commits outside of table headers in summaries

--- src/write.ts
+++ src/write.ts
@@ -220,7 +220,10 @@ export function buildComment(
         '',
         expandableDetails ? '<details>' : '',
         '',
-        `| Benchmark suite | Current: ${curSuite.commit.id} | Previous: ${prevSuite.commit.id} | Ratio |`,
+        `Previous: ${prevSuite.commit.id}`,
+        `Current: ${curSuite.commit.id}`,
+        '',
+        `| Benchmark suite | Current | Previous | Ratio |`,
         '|-|-|-|-|',
     ];
 
@@ -263,7 +266,10 @@ function buildAlertComment(
         `Possible performance regression was detected for benchmark${benchmarkText}.`,
         `Benchmark result of this commit is worse than the previous benchmark result exceeding threshold \`${thresholdString}\`.`,
         '',
-        `| Benchmark suite | Current: ${curSuite.commit.id} | Previous: ${prevSuite.commit.id} | Ratio |`,
+        `Previous commit: ${prevSuite.commit.id}`,
+        `Current commit: ${curSuite.commit.id}`,
+        '',
+        `| Benchmark suite | Current | Previous | Ratio |`,
         '|-|-|-|-|',
     ];
 
